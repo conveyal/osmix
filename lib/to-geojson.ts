2b257e0390d1d9c489e868ab1dc2b072312c6c45
Trevor Gerhardt <trevorgerhardt@gmail.com>
2025-05-14 11:20:51 +0800

    calculate bbox from nodes while generating geojson

--- lib/to-geojson.ts
+++ lib/to-geojson.ts
@@ -5,12 +5,14 @@ import {
 	parseWay,
 } from "./read-osm-pbf-blocks"
 import type {
+	Bbox,
 	OsmNode,
 	OsmPbfInfoParsed,
 	OsmPbfPrimitiveBlock,
 	OsmTags,
 	OsmWay,
 } from "./types"
+import { wayIsArea } from "./way-is-area"
 
 type OsmProperties = {
 	info?: OsmPbfInfoParsed
@@ -22,39 +24,64 @@ export type OsmGeoJSONFeature = GeoJSON.Feature<
 	OsmProperties
 >
 
-export async function* blocksToGeoJSON(
+export async function blocksToGeoJSON(
 	blocks: AsyncGenerator<OsmPbfPrimitiveBlock>,
 	opts?: ReadOptions,
-): AsyncGenerator<
-	GeoJSON.Feature<
-		GeoJSON.Point | GeoJSON.LineString | GeoJSON.Polygon,
-		OsmProperties
+): Promise<{
+	generateFeatures: AsyncGenerator<
+		GeoJSON.Feature<
+			GeoJSON.Point | GeoJSON.LineString | GeoJSON.Polygon,
+			OsmProperties
+		>
 	>
-> {
+	bbox: Bbox
+}> {
 	const nodes: Map<number, [number, number]> = new Map()
-	for await (const block of blocks) {
-		for (const group of block.primitivegroup) {
-			for (const n of group.nodes) {
-				const node = parseNode(n, block, opts)
-				nodes.set(n.id, [node.lon, node.lat])
-				if (n.keys.length > 0) {
-					yield nodeToFeature(node)
+	const bbox: Bbox = [
+		Number.POSITIVE_INFINITY,
+		Number.POSITIVE_INFINITY,
+		Number.NEGATIVE_INFINITY,
+		Number.NEGATIVE_INFINITY,
+	]
+	const updateBbox = (node: OsmNode) => {
+		bbox[0] = Math.min(bbox[0], node.lon)
+		bbox[1] = Math.min(bbox[1], node.lat)
+		bbox[2] = Math.max(bbox[2], node.lon)
+		bbox[3] = Math.max(bbox[3], node.lat)
+	}
+
+	async function* generateFeatures() {
+		for await (const block of blocks) {
+			for (const group of block.primitivegroup) {
+				for (const n of group.nodes) {
+					const node = parseNode(n, block, opts)
+					nodes.set(n.id, [node.lon, node.lat])
+					updateBbox(node)
+					if (n.keys.length > 0) {
+						yield nodeToFeature(node)
+					}
 				}
-			}
-			if (group.dense) {
-				for (const n of parseDenseNodes(group.dense, block, opts)) {
-					nodes.set(n.id, [n.lon, n.lat])
-					if (n.tags && Object.keys(n.tags).length > 0) {
-						yield nodeToFeature(n)
+				if (group.dense) {
+					for (const n of parseDenseNodes(group.dense, block, opts)) {
+						nodes.set(n.id, [n.lon, n.lat])
+						updateBbox(n)
+						if (n.tags && Object.keys(n.tags).length > 0) {
+							yield nodeToFeature(n)
+						}
+					}
+				}
+				if (group.ways) {
+					for (const w of group.ways) {
+						yield wayToFeature(parseWay(w, block, opts), nodes)
 					}
 				}
 			}
-			if (group.ways) {
-				for (const w of group.ways) {
-					yield wayToFeature(parseWay(w, block, opts), nodes)
-				}
-			}
 		}
+	}
+
+	return {
+		generateFeatures: generateFeatures(),
+		bbox,
 	}
 }
 
@@ -79,35 +106,23 @@ function wayToFeature(
 	way: OsmWay,
 	nodes: Map<number, [number, number]>,
 ): GeoJSON.Feature<GeoJSON.LineString | GeoJSON.Polygon, OsmProperties> {
-	const bbox: GeoJSON.BBox = [
-		Number.POSITIVE_INFINITY,
-		Number.POSITIVE_INFINITY,
-		Number.NEGATIVE_INFINITY,
-		Number.NEGATIVE_INFINITY,
-	]
 	const getNode = (r: number) => {
 		const n = nodes.get(r)
 		if (!n) throw new Error(`Node ${r} not found`)
-		bbox[0] = Math.min(bbox[0], n[0])
-		bbox[1] = Math.min(bbox[1], n[1])
-		bbox[2] = Math.max(bbox[2], n[0])
-		bbox[3] = Math.max(bbox[3], n[1])
 		return n
 	}
 	return {
 		type: "Feature",
 		id: way.id,
-		bbox,
-		geometry:
-			way.refs[0] === way.refs[way.refs.length - 1]
-				? {
-						type: "Polygon",
-						coordinates: [way.refs.map(getNode)],
-					}
-				: {
-						type: "LineString",
-						coordinates: way.refs.map(getNode),
-					},
+		geometry: wayIsArea(way.refs, way.tags)
+			? {
+					type: "Polygon",
+					coordinates: [way.refs.map(getNode)],
+				}
+			: {
+					type: "LineString",
+					coordinates: way.refs.map(getNode),
+				},
 		properties: {
 			info: way.info,
 			tags: way.tags,
