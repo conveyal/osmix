7e31961a02e7c85d03fd56f297b593ca1b3f29a8
Trevor Gerhardt <trevorgerhardt@gmail.com>
2025-05-23 19:12:55 +0800

    Use describe / it in tests

--- packages/osm.ts/test/read.test.ts
+++ packages/osm.ts/test/read.test.ts
@@ -1,5 +1,5 @@
 import assert from "node:assert"
-import { test } from "vitest"
+import { describe, it } from "vitest"
 
 import { createOsmPbfReadStream } from "../src/create-osm-pbf-read-stream"
 import { readOsmPbfPrimitiveBlocks } from "../src/read-osm-pbf"
@@ -7,45 +7,47 @@ import { readOsmPbfPrimitiveBlocks } from "../src/read-osm-pbf"
 import { PBFs } from "./files"
 import { getFileReadStream } from "./utils"
 
-for (const [name, pbf] of Object.entries(PBFs)) {
-	test(
-		`parse osm pbf ${name}`,
-		{
-			timeout: 100_000,
-		},
-		async () => {
-			const fileStream = await getFileReadStream(pbf.url)
-			const pbfStream = await createOsmPbfReadStream(fileStream)
-			assert.deepEqual(pbfStream.header.bbox, pbf.bbox)
+describe("parse osm pbf", () => {
+	for (const [name, pbf] of Object.entries(PBFs)) {
+		it(
+			`${name}`,
+			{
+				timeout: 100_000,
+			},
+			async () => {
+				const fileStream = await getFileReadStream(pbf.url)
+				const pbfStream = await createOsmPbfReadStream(fileStream)
+				assert.deepEqual(pbfStream.header.bbox, pbf.bbox)
 
-			let nodes = 0
-			let ways = 0
-			let relations = 0
-			for await (const entity of readOsmPbfPrimitiveBlocks(pbfStream.blocks, {
-				withTags: false,
-				withInfo: false,
-			})) {
-				if ("members" in entity) {
-					relations++
-				} else if ("refs" in entity) {
-					ways++
-				} else {
-					nodes++
+				let nodes = 0
+				let ways = 0
+				let relations = 0
+				for await (const entity of readOsmPbfPrimitiveBlocks(pbfStream.blocks, {
+					withTags: false,
+					withInfo: false,
+				})) {
+					if ("members" in entity) {
+						relations++
+					} else if ("refs" in entity) {
+						ways++
+					} else {
+						nodes++
+					}
 				}
-			}
 
-			console.log(`Total chunks: ${pbfStream.stats.chunks}`)
-			console.log(`Total blocks parsed: ${pbfStream.stats.blocks}`)
-			console.log(
-				`Total inflate time: ${(pbfStream.stats.inflateMs / 1000).toFixed(2)}s`,
-			)
-			console.log(
-				`Data size: ${(pbfStream.stats.inflateBytes / 1024 / 1024).toFixed(2)}MB`,
-			)
+				console.log(`Total chunks: ${pbfStream.stats.chunks}`)
+				console.log(`Total blocks parsed: ${pbfStream.stats.blocks}`)
+				console.log(
+					`Total inflate time: ${(pbfStream.stats.inflateMs / 1000).toFixed(2)}s`,
+				)
+				console.log(
+					`Data size: ${(pbfStream.stats.inflateBytes / 1024 / 1024).toFixed(2)}MB`,
+				)
 
-			assert.equal(nodes, pbf.nodes)
-			assert.equal(ways, pbf.ways)
-			assert.equal(relations, pbf.relations)
-		},
-	)
-}
+				assert.equal(nodes, pbf.nodes)
+				assert.equal(ways, pbf.ways)
+				assert.equal(relations, pbf.relations)
+			},
+		)
+	}
+})
--- packages/osm.ts/test/write.test.ts
+++ packages/osm.ts/test/write.test.ts
@@ -1,42 +1,47 @@
-import { assert, test } from "vitest"
+import { assert, describe, it } from "vitest"
 
 import { readOsmPbf } from "../src"
 import { osmToPrimitiveBlocks, writePbfToStream } from "../src/write-osm-pbf"
 import { PBFs } from "./files"
 import { getFileReadStream, getFileWriteStream } from "./utils"
 
-for (const [name, pbf] of Object.entries(PBFs)) {
-	test.only(
-		`write osm primitive blocks ${name}`,
-		{
-			timeout: 100_000,
-		},
-		async () => {
-			const testFileName = `${name}.test.pbf`
-			const fileStream = await getFileReadStream(pbf.url)
-			const originalOsmPbf = await readOsmPbf(fileStream, {
-				withTags: true,
-				withInfo: true,
-			})
-			// const originalBlocks = await Array.fromAsync(originalPbfReadStream.blocks)
+describe.skip("write osm primitive blocks", () => {
+	for (const [name, pbf] of Object.entries(PBFs)) {
+		it(
+			`${name}`,
+			{
+				timeout: 100_000,
+			},
+			async () => {
+				const testFileName = `${name}.test.pbf`
+				const fileStream = await getFileReadStream(pbf.url)
+				const originalOsmPbf = await readOsmPbf(fileStream, {
+					withTags: true,
+					withInfo: true,
+				})
+				// const originalBlocks = await Array.fromAsync(originalPbfReadStream.blocks)
 
-			console.time(`full stream write ${name}`)
-			const writeStream = await getFileWriteStream(testFileName)
-			await writePbfToStream(
-				writeStream,
-				originalOsmPbf.header,
-				osmToPrimitiveBlocks(originalOsmPbf),
-			)
-			console.timeEnd(`full stream write ${name}`)
+				console.time(`full stream write ${name}`)
+				const writeStream = await getFileWriteStream(testFileName)
+				await writePbfToStream(
+					writeStream,
+					originalOsmPbf.header,
+					osmToPrimitiveBlocks(originalOsmPbf),
+				)
+				console.timeEnd(`full stream write ${name}`)
 
-			const testDataStream = await getFileReadStream(testFileName)
-			const testOsmPbf = await readOsmPbf(testDataStream)
-			// const testBlocks = await Array.fromAsync(testOsmPbf.blocks)
+				const testDataStream = await getFileReadStream(testFileName)
+				const testOsmPbf = await readOsmPbf(testDataStream)
+				// const testBlocks = await Array.fromAsync(testOsmPbf.blocks)
 
-			assert.deepEqual(originalOsmPbf.header, testOsmPbf.header)
-			assert.equal(originalOsmPbf.nodes.size, testOsmPbf.nodes.size)
-			assert.equal(originalOsmPbf.ways.length, testOsmPbf.ways.length)
-			assert.equal(originalOsmPbf.relations.length, testOsmPbf.relations.length)
-		},
-	)
-}
+				assert.deepEqual(originalOsmPbf.header, testOsmPbf.header)
+				assert.equal(originalOsmPbf.nodes.size, testOsmPbf.nodes.size)
+				assert.equal(originalOsmPbf.ways.length, testOsmPbf.ways.length)
+				assert.equal(
+					originalOsmPbf.relations.length,
+					testOsmPbf.relations.length,
+				)
+			},
+		)
+	}
+})
