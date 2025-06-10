4cb48f41dfdd3853dc1550bc623d866d18487461
Trevor Gerhardt <trevorgerhardt@gmail.com>
2025-06-10 22:09:58 +0800

    Add base ULR in production

--- apps/osm-merge/vite.config.ts
+++ apps/osm-merge/vite.config.ts
@@ -1,8 +1,14 @@
+import path from "node:path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-	base: "/osm.ts/",
+	base: process.env.NODE_ENV === "production" ? "/osm.ts/" : "/",
 	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 })
