8a170675d6b2af369714277932b914961db9182c
Trevor Gerhardt <trevorgerhardt@gmail.com>
2025-08-19 06:21:19 -0400

    Ensure nav links work on production

--- apps/osm-merge/src/components/nav.tsx
+++ apps/osm-merge/src/components/nav.tsx
@@ -2,27 +2,41 @@ import CenterInfo from "@/components/center-info"
 import ZoomInfo from "@/components/zoom-info"
 import BrowserCheck from "./browser-check"
 import Status from "./status"
-import { Button } from "./ui/button"
+import { NavLink } from "react-router"
 
 export default function Nav() {
 	return (
-		<div className="border-b flex flex-row justify-between items-center">
-			<div className="flex flex-row gap-4 items-center px-4">
-				<h1 className="py-2">OSM.ts</h1>
-				<Button asChild size="sm" variant="link" className="cursor-pointer">
-					<a href="/merge">Merge</a>
-				</Button>
-				<Button asChild size="sm" variant="link" className="cursor-pointer">
-					<a href="/view">View</a>
-				</Button>
+		<div className="border-b flex flex-row justify-between items-center px-4">
+			<div className="flex flex-row gap-4 items-center py-2">
+				<div className="font-bold">OSM.ts</div>
+				<NavLink
+					className={({ isActive }) =>
+						isActive ? "text-blue-600" : "text-slate-950"
+					}
+					to="../merge"
+					relative="path"
+				>
+					Merge
+				</NavLink>
+				<NavLink
+					className={({ isActive }) =>
+						isActive ? "text-blue-600" : "text-slate-950"
+					}
+					to="../view"
+					relative="path"
+				>
+					View
+				</NavLink>
 				{process.env.NODE_ENV === "development" && <BrowserCheck />}
 			</div>
 
 			<Status />
-			<div className="flex flex-row gap-4 items-center px-4">
-				<div className="border-r pr-4">
+
+			<div className="flex flex-row gap-4 items-center">
+				<div>
 					<CenterInfo />
 				</div>
+				<hr className="border-r h-4" />
 				<div>
 					z<ZoomInfo />
 				</div>
