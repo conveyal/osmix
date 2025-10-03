7b754b6ab3645d36d7979c5e86da7996787d5942
Trevor Gerhardt <trevorgerhardt@gmail.com>
2025-10-03 16:15:59 +0800

    Remove unnecessary headers from vite config

--- apps/merge/vite.config.ts
+++ apps/merge/vite.config.ts
@@ -11,11 +11,4 @@ export default defineConfig({
 			"@": path.resolve(__dirname, "./src"),
 		},
 	},
-	server: {
-		headers: {
-			"Cross-Origin-Opener-Policy": "same-origin",
-			"Cross-Origin-Embedder-Policy": "require-corp",
-			"Cross-Origin-Resource-Policy": "same-origin",
-		},
-	},
 })
