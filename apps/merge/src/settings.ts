3b8a308fbfb04bd17ea08e90a03817c594b773ed
Trevor Gerhardt <trevorgerhardt@gmail.com>
2025-10-07 09:13:02 +0800

    Update settings.ts

--- apps/merge/src/settings.ts
+++ apps/merge/src/settings.ts
@@ -1,8 +1,8 @@
 export const APPID = "osmix"
 export const MIN_NODE_ZOOM = 10
 export const MIN_PICKABLE_ZOOM = 12
-export const DEFAULT_BASE_PBF_URL = "./pbfs/yakima-full.osm.pbf"
-export const DEFAULT_PATCH_PBF_URL = "./pbfs/yakima-osw.osm.pbf"
+export const DEFAULT_BASE_PBF_URL = "./yakima-full.osm.pbf"
+export const DEFAULT_PATCH_PBF_URL = "./yakima-osw.osm.pbf"
 
 export const RASTER_PROTOCOL_NAME = "@osmix/raster"
 export const RASTER_TILE_SIZE = 256
