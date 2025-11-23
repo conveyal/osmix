595de457fd6b8959bd37f4c8f9d361b3b315a119
Trevor Gerhardt <trevorgerhardt@gmail.com>
2025-11-23 17:21:30 +0800

    Update memory.test.ts

--- packages/core/test/memory.test.ts
+++ packages/core/test/memory.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from "vitest"
+import { describe, expect, it } from "bun:test"
 import { Nodes, Relations, Ways } from "../src"
 
 export function getOsmCoreMemoryRequirements(
