fb405db76953721bda1dc6aa538cd91b9b63a16a
Trevor Gerhardt <trevorgerhardt@gmail.com>
2025-12-13 01:14:10 +0400

    Update Vite

--- apps/merge/vite.config.ts
+++ apps/merge/vite.config.ts
@@ -1,4 +1,3 @@
-import path from "node:path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
@@ -8,12 +7,6 @@ export default defineConfig({
 	plugins: [react(), tailwindcss()],
 	publicDir:
 		process.env.NODE_ENV === "development" ? "../../fixtures" : undefined,
-	resolve: {
-		alias: {
-			react: path.resolve(__dirname, "node_modules/react"),
-			"react-dom": path.resolve(__dirname, "node_modules/react-dom"),
-		},
-	},
 	server: {
 		headers: {
 			"Cross-Origin-Embedder-Policy": "require-corp",
